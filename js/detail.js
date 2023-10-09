a5f2619c2303e249c5bdcd52ea5483acd1d1d4e7
milqtbear <milqtbear@gmail.com>
2023-10-09 15:58:23 +0900

    feat 작품 설명페이지

---
+++ js/detail.js
@@ -0,0 +1,8 @@
+$(function () {
+    $('.e-floating-btn').click(function () {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" // 부드러운 스크롤 적용
+        });
+    });
+  });
