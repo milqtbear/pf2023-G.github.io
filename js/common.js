8f9d7dc3c28d5062297e979907e861c0c8d95a40
milqtbear <milqtbear@gmail.com>
2023-10-09 17:25:43 +0900

    feat: 헤더적용

--- js/common.js
+++ js/common.js
@@ -1,6 +1,6 @@
 $(function () {
   $('.e-header__btn-close').click(function () {
-    location.href= './details/1.html'; // TODO: 지도로 경로 수정
+    location.href= './../map/index.html'; // TODO: 지도로 경로 수정
   });
 
   $('.e-header__btn-menu').click(function () {
