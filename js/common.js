af6e1afad2d5f32ec96b84969ebd70a76b943635
ggosugosu <gohsue0@gmail.com>
2023-10-18 00:49:02 +0900

    feat: 메뉴 애니메이션 추가

--- js/common.js
+++ js/common.js
@@ -6,12 +6,12 @@ $(function () {
   });
 
   $('.e-header__btn-menu').click(function () {
-    $('.e-header__drawer-container').toggleClass('hidden');
+    $('.e-header__drawer-container').toggleClass('aside-hidden');
     $('.header__btn-aside-close').toggleAttribute('aria-hidden', 'false');
   });
 
   $('.e-header__btn-aside-close').click(function () {
-    $('.e-header__drawer-container').toggleClass('hidden');
+    $('.e-header__drawer-container').toggleClass('aside-hidden');
     $('.header__btn-aside-close').toggleAttribute('aria-hidden', 'true');
   });
 });
